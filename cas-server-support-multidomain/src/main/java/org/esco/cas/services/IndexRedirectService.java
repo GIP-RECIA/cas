f30120fae00bfce764609b1e15990e5c4293a696
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2020-01-15 19:37:12 +0100

    feat: add property to set a Map with user attribute and values to check to forbide access to service

--- cas-server-support-multidomain/src/main/java/org/esco/cas/services/IndexRedirectService.java
+++ cas-server-support-multidomain/src/main/java/org/esco/cas/services/IndexRedirectService.java
@@ -1,130 +1,81 @@
- package org.esco.cas.services;
- 
- import java.util.regex.Matcher;
- import java.util.regex.Pattern;
- import org.apache.commons.logging.Log;
- import org.apache.commons.logging.LogFactory;
- import org.jasig.cas.authentication.principal.Service;
- import org.jasig.cas.services.RegisteredServiceImpl;
- import org.springframework.beans.factory.InitializingBean;
- import org.springframework.util.Assert;
- import org.springframework.util.StringUtils;
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- public class IndexRedirectService
-   extends RegisteredServiceImpl
-   implements InitializingBean
- {
-   private static final long serialVersionUID = -3601483389214507542L;
-   private static final Log LOGGER = LogFactory.getLog(IndexRedirectService.class);
-   
- 
- 
- 
-   private String indexRedirectUrl;
-   
- 
- 
- 
-   private String indexCookieName;
-   
- 
- 
- 
-   private boolean forceRedirection = true;
-   
- 
-   public boolean matches(Service service)
-   {
-     if (service != null) {
-       Pattern p = Pattern.compile(getServiceId(), 2);
-       Matcher m = p.matcher(service.getId());
-       boolean result = m.find();
-       LOGGER.debug("Is service matching between " + getServiceId() + " and " + service.getId() + " returned " + result);
-       if (result) {
-         return true;
-       }
-     }
-     return false;
-   }
-   
-   public void afterPropertiesSet() throws Exception
-   {
-     Assert.isTrue((!StringUtils.hasText(this.indexRedirectUrl)) || (StringUtils.hasText(this.indexCookieName)), "Index redirection config need a URL and a cookie name !");
-   }
-   
- 
- 
- 
- 
- 
- 
-   public String getIndexRedirectUrl()
-   {
-     return this.indexRedirectUrl;
-   }
-   
- 
- 
- 
- 
- 
-   public void setIndexRedirectUrl(String indexRedirectUrl)
-   {
-     this.indexRedirectUrl = indexRedirectUrl;
-   }
-   
- 
- 
- 
- 
-   public String getIndexCookieName()
-   {
-     return this.indexCookieName;
-   }
-   
- 
- 
- 
- 
-   public void setIndexCookieName(String indexCookieName)
-   {
-     this.indexCookieName = indexCookieName;
-   }
-   
- 
- 
- 
- 
- 
-   public boolean isForceRedirection()
-   {
-     return this.forceRedirection;
-   }
-   
- 
- 
- 
- 
- 
-   public void setForceRedirection(boolean forceRedirection)
-   {
-     this.forceRedirection = forceRedirection;
-   }
- }
+package org.esco.cas.services;
 
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasig.cas.authentication.principal.Service;
+import org.jasig.cas.services.RegisteredServiceImpl;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
-/* Location:              /home/jgribonvald/Téléchargements/cas-server-support-multidomain-1.1.2.jar!/org/esco/cas/services/IndexRedirectService.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       0.7.1
- */
+public class IndexRedirectService extends RegisteredServiceImpl implements InitializingBean {
+    private static final long serialVersionUID = -3601483389214507542L;
+    private static final Log LOGGER = LogFactory.getLog(IndexRedirectService.class);
+
+    private String indexRedirectUrl;
+
+    private String indexCookieName;
+
+    private boolean forceRedirection = true;
+
+    private Map<String, List<String>> filteredUsers;
+
+    public boolean matches(Service service) {
+        if (service != null) {
+            Pattern p = Pattern.compile(getServiceId(), 2);
+            Matcher m = p.matcher(service.getId());
+            boolean result = m.find();
+            LOGGER.debug("Is service matching between " + getServiceId() + " and " + service.getId() + " returned " + result);
+            if (result) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        Assert.isTrue((!StringUtils.hasText(this.indexRedirectUrl)) || (StringUtils.hasText(this.indexCookieName)), "Index redirection config need a URL and a cookie name !");
+    }
+
+    public String getIndexRedirectUrl()
+    {
+        return this.indexRedirectUrl;
+    }
+
+    public void setIndexRedirectUrl(String indexRedirectUrl)
+    {
+        this.indexRedirectUrl = indexRedirectUrl;
+    }
+
+    public String getIndexCookieName()
+    {
+        return this.indexCookieName;
+    }
+
+    public void setIndexCookieName(String indexCookieName)
+    {
+        this.indexCookieName = indexCookieName;
+    }
+
+    public boolean isForceRedirection()
+    {
+        return this.forceRedirection;
+    }
+
+    public void setForceRedirection(boolean forceRedirection)
+    {
+        this.forceRedirection = forceRedirection;
+    }
+
+    public Map<String, List<String>> getFilteredUsers() {
+        return filteredUsers;
+    }
+
+    public void setFilteredUsers(Map<String, List<String>> filteredUsers) {
+        this.filteredUsers = filteredUsers;
+    }
+}
--- cas-server-support-multidomain/src/main/java/org/esco/cas/services/MultiDomainService.java
+++ cas-server-support-multidomain/src/main/java/org/esco/cas/services/MultiDomainService.java
@@ -1,129 +1,95 @@
- package org.esco.cas.services;
- 
- import java.util.List;
- import java.util.Set;
- import java.util.regex.Matcher;
- import java.util.regex.Pattern;
- import org.apache.commons.logging.Log;
- import org.apache.commons.logging.LogFactory;
- import org.esco.cas.CasHelper;
- import org.esco.cas.multidomain.IMultiDomainConfig;
- import org.jasig.cas.authentication.principal.Service;
- import org.springframework.beans.factory.InitializingBean;
- import org.springframework.util.Assert;
- import org.springframework.util.StringUtils;
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- public class MultiDomainService
-   extends IndexRedirectService
-   implements InitializingBean
- {
-   private static final long serialVersionUID = 1022678474253813028L;
-   private static final Log LOGGER = LogFactory.getLog(MultiDomainService.class);
-   
-   private transient IMultiDomainConfig multiDomainConfig;
-   
- 
-   public String getTheme()
-   {
-     return this.multiDomainConfig.getCurrentTheme();
-   }
-   
-   public boolean matches(Service service)
-   {
-     boolean result = false;
-     
-     String baseServiceId = super.getServiceId();
-     String serviceDomain = null;
-     if (service != null) {
-       serviceDomain = CasHelper.extractDomainName(service.getId());
-       
-       if ((serviceDomain != null) && (this.multiDomainConfig.getDomains().contains(serviceDomain)))
-       {
- 
-         String adaptedServiceId = CasHelper.replaceUrlDomain(baseServiceId, serviceDomain);
-         
-         Pattern p = Pattern.compile(adaptedServiceId, 2);
-         Matcher m = p.matcher(service.getId());
-         result = m.find();
-         
-         if (LOGGER.isDebugEnabled()) {
-           LOGGER.debug(String.format("Is M-D service (%d) matching between [%s] and [%s] returned [%s].", new Object[] { Long.valueOf(getId()), adaptedServiceId, service.getId(), Boolean.valueOf(result) }));
-         }
-         
-       }
-       else if (LOGGER.isDebugEnabled()) {
-         LOGGER.debug(String.format("M-D Service (%1$d) [%2$s] is not authorized for service's domain name of [%3$s] returned [%4$s].", new Object[] { Long.valueOf(getId()), baseServiceId, service.getId(), Boolean.valueOf(result) }));
-       }
-     }
-     
- 
- 
-     return result;
-   }
-   
-   public String getServiceId()
-   {
-     String resultId = null;
-     
-     String baseServiceId = super.getServiceId();
-     
- 
-     String currentServiceId = this.multiDomainConfig.getCurrentServiceId(baseServiceId);
-     if (StringUtils.hasText(currentServiceId)) {
-       resultId = currentServiceId;
-     } else {
-       resultId = baseServiceId;
-     }
-     
- 
-     return resultId;
-   }
-   
-   public String getIndexRedirectUrl()
-   {
-     String redirectionUrl = this.multiDomainConfig.getOverrideIndexRedirectUrl();
-     
-     if (!StringUtils.hasText(redirectionUrl))
-     {
-       String currentDomain = this.multiDomainConfig.getCurrentDomainName();
-       redirectionUrl = CasHelper.replaceUrlDomain(super.getIndexRedirectUrl(), currentDomain);
-     }
-     
-     return redirectionUrl;
-   }
-   
-   public void afterPropertiesSet() throws Exception
-   {
-     Assert.notNull(this.multiDomainConfig, "A multidomain Service need a configuration !");
-     
-     LOGGER.info(String.format("Multi-domain service [%s] loaded with base service Id = [%s], authorized domains(%s) = [%s], allowed attributes(%s) = [%s].", new Object[] { Long.valueOf(getId()), getServiceId(), Integer.valueOf(this.multiDomainConfig.getDomains().size()), this.multiDomainConfig.getDomains(), Integer.valueOf(getAllowedAttributes().size()), getAllowedAttributes() }));
-   }
-   
- 
- 
- 
-   public IMultiDomainConfig getMultiDomainConfig()
-   {
-     return this.multiDomainConfig;
-   }
-   
-   public void setMultiDomainConfig(IMultiDomainConfig multiDomainConfig) {
-     this.multiDomainConfig = multiDomainConfig;
-   }
- }
+package org.esco.cas.services;
 
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.esco.cas.CasHelper;
+import org.esco.cas.multidomain.IMultiDomainConfig;
+import org.jasig.cas.authentication.principal.Service;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
-/* Location:              /home/jgribonvald/Téléchargements/cas-server-support-multidomain-1.1.2.jar!/org/esco/cas/services/MultiDomainService.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       0.7.1
- */
+public class MultiDomainService extends IndexRedirectService implements InitializingBean {
+  private static final long serialVersionUID = 1022678474253813028L;
+  private static final Log LOGGER = LogFactory.getLog(MultiDomainService.class);
+
+  private transient IMultiDomainConfig multiDomainConfig;
+
+  public String getTheme()
+  {
+    return this.multiDomainConfig.getCurrentTheme();
+  }
+
+  public boolean matches(Service service) {
+    boolean result = false;
+
+    String baseServiceId = super.getServiceId();
+    String serviceDomain = null;
+    if (service != null) {
+      serviceDomain = CasHelper.extractDomainName(service.getId());
+
+      if ((serviceDomain != null) && (this.multiDomainConfig.getDomains().contains(serviceDomain))) {
+
+        String adaptedServiceId = CasHelper.replaceUrlDomain(baseServiceId, serviceDomain);
+
+        Pattern p = Pattern.compile(adaptedServiceId, 2);
+        Matcher m = p.matcher(service.getId());
+        result = m.find();
+
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug(String.format("Is M-D service (%d) matching between [%s] and [%s] returned [%s].", new Object[] { Long.valueOf(getId()), adaptedServiceId, service.getId(), Boolean.valueOf(result) }));
+        }
+
+      }
+      else if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug(String.format("M-D Service (%1$d) [%2$s] is not authorized for service's domain name of [%3$s] returned [%4$s].", new Object[] { Long.valueOf(getId()), baseServiceId, service.getId(), Boolean.valueOf(result) }));
+      }
+    }
+
+    return result;
+  }
+
+  public String getServiceId() {
+    String resultId = null;
+
+    String baseServiceId = super.getServiceId();
+
+    String currentServiceId = this.multiDomainConfig.getCurrentServiceId(baseServiceId);
+    if (StringUtils.hasText(currentServiceId)) {
+      resultId = currentServiceId;
+    } else {
+      resultId = baseServiceId;
+    }
+    return resultId;
+  }
+
+  public String getIndexRedirectUrl() {
+    String redirectionUrl = this.multiDomainConfig.getOverrideIndexRedirectUrl();
+
+    if (!StringUtils.hasText(redirectionUrl))
+    {
+      String currentDomain = this.multiDomainConfig.getCurrentDomainName();
+      redirectionUrl = CasHelper.replaceUrlDomain(super.getIndexRedirectUrl(), currentDomain);
+    }
+
+    return redirectionUrl;
+  }
+
+  public void afterPropertiesSet() throws Exception {
+    Assert.notNull(this.multiDomainConfig, "A multidomain Service need a configuration !");
+
+    LOGGER.info(String.format("Multi-domain service [%s] loaded with base service Id = [%s], authorized domains(%s) = [%s], allowed attributes(%s) = [%s].", new Object[] { Long.valueOf(getId()), getServiceId(), Integer.valueOf(this.multiDomainConfig.getDomains().size()), this.multiDomainConfig.getDomains(), Integer.valueOf(getAllowedAttributes().size()), getAllowedAttributes() }));
+  }
+
+  public IMultiDomainConfig getMultiDomainConfig()  {
+    return this.multiDomainConfig;
+  }
+
+  public void setMultiDomainConfig(IMultiDomainConfig multiDomainConfig) {
+    this.multiDomainConfig = multiDomainConfig;
+  }
+}
